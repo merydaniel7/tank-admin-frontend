1473dde5e2475bd65f0bb65759759e0658141391
merydaniel7 <merydaniel7@gmail.com>
2022-01-27 08:48:48 +0100

    Add: temporarily commented it out

--- src/views/admin/Tables.js
+++ src/views/admin/Tables.js
@@ -2,19 +2,19 @@ import React from "react";
 
 // components
 
-import CardTable from "components/Cards/CardTable.js";
+// import CardTable from "components/Cards/CardTable.js";
 
 export default function Tables() {
   return (
     <>
-      <div className="flex flex-wrap mt-4">
+      {/* <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
           <CardTable />
         </div>
         <div className="w-full mb-12 px-4">
           <CardTable color="dark" />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
