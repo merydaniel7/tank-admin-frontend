4da728087121771d92dee15bf6c9a69fa703651c
merydaniel7 <merydaniel7@gmail.com>
2022-02-10 15:49:27 +0100

    Add: comment it out

--- src/views/admin/Dashboard.js
+++ src/views/admin/Dashboard.js
@@ -1,17 +1,17 @@
-import React from "react";
+// import React from "react";
 
-// components
+// // components
 
-import CardLineChart from "components/Cards/CardLineChart.js";
-import CardBarChart from "components/Cards/CardBarChart.js";
-import CardPageVisits from "components/Cards/CardPageVisits.js";
-import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+// import CardLineChart from "components/Cards/CardLineChart.js";
+// import CardBarChart from "components/Cards/CardBarChart.js";
+// import CardPageVisits from "components/Cards/CardPageVisits.js";
+// import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
 
 export default function Dashboard() {
   
   return (
     <>
-      <div className="flex flex-wrap">
+      {/* <div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
           <CardLineChart />
         </div>
@@ -26,7 +26,7 @@ export default function Dashboard() {
         <div className="w-full xl:w-4/12 px-4">
           <CardSocialTraffic />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
